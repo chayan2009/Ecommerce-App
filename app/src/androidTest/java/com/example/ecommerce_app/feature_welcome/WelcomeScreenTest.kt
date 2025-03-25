bcea919cc1342bac28742f4effcd5e9d6e2d6ff9
chayan <chayanchowdhury@gmail.com>
2025-03-25 19:59:06 +0530

    integration and instrumentaiton test added

--- app/src/androidTest/java/com/example/ecommerce_app/feature_welcome/WelcomeScreenTest.kt
+++ app/src/androidTest/java/com/example/ecommerce_app/feature_welcome/WelcomeScreenTest.kt
@@ -1,27 +1,18 @@
 package com.example.ecommerce_app.feature_welcome
 
-import android.content.Context
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
-import com.example.clean_architecture_android.ui.theme.CleanArchitectureAndroidTheme
-import com.example.ecommerce_app.HiltTestActivity
-import com.example.ecommerce_app.feature_login.LoginScreen
-import com.example.ecommerce_app.feature_login.viewmodel.LoginViewModel
-import com.example.ecommerce_app.feature_signup.SignUpViewModel
-import com.example.ecommerce_app.feature_signup.presentation.SignUpScreen
+import com.example.ecommerce_app.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+
 @HiltAndroidTest
 class WelcomeScreenTest {
 
@@ -29,42 +20,29 @@ class WelcomeScreenTest {
     val hiltRule = HiltAndroidRule(this)
 
     @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<HiltTestActivity>()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     private lateinit var navController: TestNavHostController
 
     @Before
-    fun setUp() {
+    fun setup() {
         hiltRule.inject()
-
-        composeTestRule.setContent {
-            CleanArchitectureAndroidTheme {
-                navController = TestNavHostController(composeTestRule.activity).apply {
-                    navigatorProvider.addNavigator(ComposeNavigator())
-                }
-
-                NavHost(
-                    navController = navController,
-                    startDestination = Screen.WelcomeScreen.route
-                ) {
-                    composable(Screen.WelcomeScreen.route) { WelcomeScreen(navController) }
-                    composable(Screen.LoginScreen.route) {
-                        val viewModel = hiltViewModel<LoginViewModel>()
-                        LoginScreen(navController, viewModel)
-                    }
-                    composable(Screen.SignUpScreen.route) {
-                        val viewModel = hiltViewModel<SignUpViewModel>()
-                        SignUpScreen(navController, viewModel)
-                    }
-                }
-            }
-        }
     }
 
     @Test
-    fun click_signUpButton_navigatesToSignUpScreen() {
+    fun welcomeScreen_displaysTextAndButtons() {
+        composeTestRule.onNodeWithText("Welcome to Ecommerce App").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("SignInButton").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("SignUpButton").assertIsDisplayed()
+    }
+
+    @Test
+    fun clickingSignInButton_navigatesToLoginScreen() {
+        composeTestRule.onNodeWithTag("SignInButton").performClick()
+    }
+
+    @Test
+    fun clickingSignUpButton_navigatesToSignUpScreen() {
         composeTestRule.onNodeWithTag("SignUpButton").performClick()
-        composeTestRule.waitForIdle()
-        assert(navController.currentDestination?.route == Screen.SignUpScreen.route)
     }
 }
